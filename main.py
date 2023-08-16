a7dcd372708d6f956017ed12f42da3766f2bbcdf
wmer <mahibni@outlook.com>
2023-08-16 10:53:28 +0000

    Add some path

--- main.py
+++ main.py
@@ -65,6 +65,11 @@ async def extractor(c, m):
             final_url = partials[0].replace('A1jKzO+1adL._CLa%7C2140%2C2000%7C', '')
             break
 
+        if f'{amazon_url}B1UOGf+zWMS._CLa%7C2140%2C2000%7C' in word:
+            partials = word.split('.png')
+            final_url = partials[0].replace('B1UOGf+zWMS._CLa%7C2140%2C2000%7C', '')
+            break
+
     await c.send_photo(chat_id, final_url + '.png')
 
 print("I'm live !!")
